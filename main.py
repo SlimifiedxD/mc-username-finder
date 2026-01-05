e5c7974221927d95f2ae4a45f5c8e4b51b400d0a
Aaryn Tiger Malik <godofthemonki@gmail.com>
2026-01-05 00:04:37 -0800

    Send more API requests

--- main.py
+++ main.py
@@ -1,47 +1,64 @@
-from mojang import API, TooManyRequests, NotFound
 import random
 import string
 import time
 import itertools
+from typing import List
+import requests
+from requests import RequestException
 
 # all allowed characters in Minecraft username
 CHARACTER_POOL = string.ascii_lowercase + string.digits + '_'
-COMBO_LEN = 4
+COMBO_LEN = 3
 
-GLOBAL_DELAY = 1.0
-MAX_DELAY = 60.0
+class MojangAPIError(Exception):
+    pass
 
-api = API()
+def make_api_request(initial_names: List[str]) -> List[str]:
+    if len(initial_names) > 10:
+        raise Exception("List is too large")
+    try:
+        response = requests.post('https://api.mojang.com/profiles/minecraft', json=initial_names,
+        headers={
+            'Content-Type': 'application/json'
+        })
+        if response.status_code != 200:
+            print(f'Error response: {response.text}')
+            return []
+        data = response.json()
+        if isinstance(data, dict) and 'error' in data:
+            raise MojangAPIError(data.get('errorMessage'))
+        if isinstance(data, str):
+            raise MojangAPIError(f'API error returned string: {data}')
+        if not isinstance(data, list):
+            raise MojangAPIError(f'Unexpected response type: {type(data)}')
 
-def get_uuid_safe(name):
-    global GLOBAL_DELAY
+        initial_set = {name.lower() for name in initial_names}
+        api_names = {item['name'].lower() for item in data}
+        return list(initial_set - api_names)
 
-    while True:
-        try:
-            uuid = api.get_uuid(name)
-            time.sleep(GLOBAL_DELAY)
-            return uuid
-        except NotFound:
-            time.sleep(GLOBAL_DELAY)
-            return None
-        except TooManyRequests:
-            print(f'Rate Limitied. Sleeping {GLOBAL_DELAY:.2f}s')
-            time.sleep(GLOBAL_DELAY)
-            GLOBAL_DELAY = min(GLOBAL_DELAY * 2, MAX_DELAY)
+    except MojangAPIError as e:
+        print(f'Mojang rejected request: {e}')
+        return []
+
+    except RequestException as e:
+        print(f'HTTP/Network Error: {e}')
+        return []
 
 def all_usernames():
     for combo in itertools.product(CHARACTER_POOL, repeat=COMBO_LEN):
         yield ''.join(combo)
 
 def create_all_usernames():
-    for name in all_usernames():
-        uuid = get_uuid_safe(name)
-        if uuid:
-            print(f'Found Taken Name: {name}')
-            continue
-        print(f'Found Not Taken Name: {name}')
-        with open('not-taken-names.txt', 'a') as file:
-            file.write(name + '\n')
+    names = list(all_usernames())
+    random.shuffle(names)
+    segment_lists = [names[i:i+10] for i in range(0, len(names), 10)]
+    for l in segment_lists:
+        not_taken_names = make_api_request(l)
+        for name in not_taken_names:
+            print(f'Found Name: {name}')
+            with open('names.txt', 'a') as file:
+                file.write(name + '\n')
+        time.sleep(1.25)
 
 def create_random_username(length):
     return ''.join(random.choices(CHARACTER_POOL, k=length))
